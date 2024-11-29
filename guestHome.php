3cde80c36be82266962f7fe7ee517b141c85eab6
gambaLunatica <juanaluna553@gmail.com>
2024-11-29 18:14:49 +0100

    Empezando guestHome

---
+++ guestHome.php
@@ -0,0 +1,29 @@
+<div class = "topDesc">
+    <div class = "textDesc">
+        <h1>¡BIENVENIDO A TOTCLOUD!</h1>
+        <p>Somos una compañia bla bla y orecemos servisios de bla bla </p>
+    </div>
+    <!-- Insertar Imagen -->
+</div>
+<div class = "catalog">
+    <div class = "produt">
+        <!-- Insertar Imagen -->
+         <h3>Virtual Machines</h3>
+         <p>Las maquinas virtuales son conocidas por bla bla bla</p>
+    </div>
+    <div class = "produt">
+        <!-- Insertar Imagen -->
+         <h3>Storages</h3>
+         <p>Con nuestro sistema de almacenamiento podrá bla bla</p>
+    </div>
+    <div class = "produt">
+        <!-- Insertar Imagen -->
+         <h3>Data Bases</h3>
+         <p>Ofrecemes diferentes configuraciones para las bases de datos bla bla</p>
+    </div>
+    <div class = "product">
+        <!-- Insertar Imagen -->
+         <h3>Virtual Network</h3>
+         <p>Las Virtual Networks bl bla</p>
+    </div>
+</div>
