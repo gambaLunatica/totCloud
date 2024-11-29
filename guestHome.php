c2ffc3debbba10b5d65ab214aeb9d63e44b2ec91
gambaLunatica <juanaluna553@gmail.com>
2024-11-29 20:48:02 +0100

    guestHome acabado

--- guestHome.php
+++ guestHome.php
@@ -1,30 +1,30 @@
 <div class = "topDesc">
     <div class = "textDesc">
-        <h1>¡BIENVENIDO A TOTCLOUD!</h1>
-        <p>Somos una compañia bla bla y orecemos servisios de bla bla </p>
+        <h1>¡WELCOME TO TOTCLOUD!</h1>
+        <p> TotCloud specializes in delivering cutting-edge Platform as a Service (PaaS) and Software as a Service (SaaS) solutions. We empower businesses by providing scalable, reliable, and user-friendly cloud-based services designed to streamline operations, enhance productivity, and enable innovation. Our platform is tailored to meet the demands of modern organizations, ensuring seamless integration and maximum efficiency. </p>
     </div>
     <!-- Insertar Imagen -->
-     <!--img src = "totCloud.ico" alt="Princilpal Image"-->
+     <img src = "totCloud.jpg" alt="Princilpal Image" class="imageDesc">
 </div>
 <div class = "catalog">
-    <div class = "produt">
-        <!-- Insertar Imagen -->
+    <div class = "product">
+        <img src = "https://media.istockphoto.com/id/162488938/es/foto/c%C3%B3digo-binario-y-monitores-de-ordenadores.jpg?s=612x612&w=0&k=20&c=2KyjtxxbZlTemdm4I2uLSPtKqc5xsuyEPa9vuwQvCH0=" alt="MV" class="product img">
          <h3>Virtual Machines</h3>
-         <p>Las maquinas virtuales son conocidas por bla bla bla</p>
-    </div>
-    <div class = "produt">
-        <!-- Insertar Imagen -->
-         <h3>Storages</h3>
-         <p>Con nuestro sistema de almacenamiento podrá bla bla</p>
-    </div>
-    <div class = "produt">
-        <!-- Insertar Imagen -->
-         <h3>Data Bases</h3>
-         <p>Ofrecemes diferentes configuraciones para las bases de datos bla bla</p>
+         <p>Virtual Machines offer a powerful and flexible computing environment that allows you to run applications, test software, or deploy systems in isolated and scalable virtualized servers</p>
     </div>
     <div class = "product">
-        <!-- Insertar Imagen -->
+        <img src="https://media.istockphoto.com/id/1254718662/es/foto/tecnolog%C3%ADa-de-computaci%C3%B3n-en-la-nube-y-almacenamiento-de-datos-en-l%C3%ADnea-para-el-concepto-de.jpg?s=612x612&w=0&k=20&c=YKkf-ZVfnT0X2w_WXAfri9zKaIC1IRnqUoPHGLoPfDU=" alt="Storage" class="product img">
+         <h3>Storages</h3>
+         <p>Our storage solutions provide secure and scalable options for storing and managing your data. Perfect for backups, collaboration, and seamless accessibility across devices</p>
+    </div>
+    <div class = "product">
+        <img src="https://media.istockphoto.com/id/1399944678/es/foto/se-coloca-varias-bases-de-datos-en-tablas-de-bases-de-datos-relacionales-con-sala-de.jpg?s=612x612&w=0&k=20&c=-WzVYH2l8haNOOXD5ErkD2UFScCdjUCNDKO72HTP3DQ=" alt="dataBases" class="product img">
+         <h3>Data Bases</h3>
+         <p>Discover our highly configurable database solutions designed to handle structured and unstructured data. They ensure fast querying, reliability, and support for your data-driven applications</p>
+    </div>
+    <div class = "product">
+         <img src = "virtualNetworks.jpg" alt="virtualNetwork" class="product img">
          <h3>Virtual Network</h3>
-         <p>Las Virtual Networks bl bla</p>
+         <p>Build and manage secure and efficient virtual networks to enhance connectivity between your systems. Ideal for optimizing communication in cloud-based environments</p>
     </div>
 </div>
